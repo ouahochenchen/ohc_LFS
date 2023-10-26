bc04d4c20a6d670cd94e3d028765fadf94e8ca70
ouhaochen <2571727427@qq.com>
2023-10-26 22:47:36 +0800

    xixi

--- internal/domain/order/order_domain.go
+++ internal/domain/order/order_domain.go
@@ -1,1 +1,14 @@
 package order
+
+type OrderDomain interface {
+	CheckDup(ormOrderId uint64)
+}
+type oderDomainImpl struct {
+}
+
+func NewDomainImpl() OrderDomain {
+	return &oderDomainImpl{}
+}
+func (*oderDomainImpl) CheckDup(ormOrderId uint64) {
+
+}
--- protocol/admin/order_protocol.go
+++ protocol/admin/order_protocol.go
@@ -1,1 +1,9 @@
 package admin
+
+type CheckDuplicateRequest struct {
+	OrmOrderId uint64
+}
+type CheckDuplicateResponse struct {
+	OrmOrderId uint64
+	IsOk       bool
+}
