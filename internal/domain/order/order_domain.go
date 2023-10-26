0248aba3e4aa2357d9df277fd05930858d6e1c54
ouhaochen <2571727427@qq.com>
2023-10-26 23:47:19 +0800

    二次提交，嘻嘻

--- internal/domain/order/order_domain.go
+++ internal/domain/order/order_domain.go
@@ -1,7 +1,9 @@
 package order
 
+import "LFS/internal/dal/repositry/order_repo"
+
 type OrderDomain interface {
-	CheckDup(ormOrderId uint64)
+	CheckDup(ormOrderId uint64) (bool, error)
 }
 type oderDomainImpl struct {
 }
@@ -9,6 +11,13 @@ type oderDomainImpl struct {
 func NewDomainImpl() OrderDomain {
 	return &oderDomainImpl{}
 }
-func (*oderDomainImpl) CheckDup(ormOrderId uint64) {
-
+func (*oderDomainImpl) CheckDup(ormOrderId uint64) (bool, error) {
+	id, err := order_repo.NewOrderRepo().SelectById(ormOrderId)
+	if err != nil {
+		return false, err
+	}
+	if id != nil {
+		return false, nil
+	}
+	return true, nil
 }
