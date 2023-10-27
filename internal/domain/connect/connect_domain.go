9bebaf371ec801d8d4973e60d4bf591bbf2eb55d
ouhaochen <2571727427@qq.com>
2023-10-27 16:03:45 +0800

    xixi

--- internal/domain/connect/connect_domain.go
+++ internal/domain/connect/connect_domain.go
@@ -1,1 +1,31 @@
 package connect
+
+import (
+	"LFS/internal/dal/repositry/ls_connect_repo"
+	"LFS/protocol/admin"
+)
+
+var repo = ls_connect_repo.NewConnectRepo()
+
+type ConnectDomain interface {
+	Create(*admin.CreateConnectRequest) (uint64, error)
+}
+type connectDomainImpl struct {
+}
+
+func NewConnectDomain() ConnectDomain {
+	return &connectDomainImpl{}
+}
+func (c *connectDomainImpl) Create(req *admin.CreateConnectRequest) (uint64, error) {
+	tab := ls_connect_repo.LaneSiteConnectConfigurationTab{
+		ResourceId:     req.ResourceId,
+		NextResourceId: req.NextResourceId,
+		ResourceType:   req.ResourceType,
+		NextType:       req.NextType,
+	}
+	createId, err := repo.Create(&tab)
+	if err != nil {
+		return 0, err
+	}
+	return createId, nil
+}
